8ef5c4125ccc91fa6ceea49d7da21cb5ce9e5968
ljtn <jonathan.lampa1@gmail.com>
2016-11-06 18:39:59 +0100

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,14 @@
+<!-- HTML END TAGS -->
+        </div> <!-- PAGE -->
+        <footer class="row">
+            <div class="col-md-4">
+            </div>
+            <div class="footermiddle col-md-4">
+                <p>All Rights Reserved 2016</p>
+                <p>Developed by: Loopex</p>
+            </div>
+            <div class="col-md-4">
+            </div>
+        </footer>
+    </body>
+</html>
---
+++ header.php
@@ -0,0 +1,52 @@
+
+<!--*************************************************************************** 
+    STANDARD-HTML AND PHP TO INCLUDE ON PAGES.
+    CONTAINS HEADER AND BODY.
+    ALSO CONTAINS STANDARD PHP VIEWING DIFFERENTLY IF LOGGED IN
+    INCLUDES BOOTSTRAP
+     ***********************************************************************-->
+
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bloggprojekt</title>
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- ------------------------------------------------------------------------
+    // BOOTSTRAP
+    ------------------------------------------------------------------------ -->
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</head>
+
+<body>
+
+    <header class="container-fluid">
+        <nav id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Hem</a></li>
+            <li><a href="#about">Blogg</a></li>
+            <li><a href="#contact">Statistik</a></li>
+          </ul>
+        </nav>
+    </header>
+    <div class="page row">
+            <div class="col-md-1">
+            </div>
+            <div class="feed col-md-10">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Dignissimos tenetur quidem qui sit tempore ratione nobis sequi
+                 saepe impedit alias voluptate iusto rerum incidunt doloremque,
+                est amet dolorum quam quibusdam!</p>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Dignissimos tenetur quidem qui sit tempore ratione nobis sequi
+                 saepe impedit alias voluptate iusto rerum incidunt doloremque,
+                est amet dolorum quam quibusdam!</p>
+            </div>
+            <div class="col-md-1">
+            </div>
