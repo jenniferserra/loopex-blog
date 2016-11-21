d3eaa2e78e371447c7d414cd14e0f92c161cd4dc
jenniferserra <jennifer.serra@medieinstitutet.se>
2016-11-21 14:51:40 +0100

    skapat post.php där man ska kunna kommentera. på index.php har vi lagt till en kommentera-länk

--- post.php
+++ post.php
@@ -1,18 +1,18 @@
 <?php
-require "header.php";
-?>
 
-<div class="banner"> </div>
-<div class="col-md-2"></div> <div class="col-md-8">
-    
-<?php
+
+//include "header.php";
+include "dbconnect.php";
+
+var_dump($_GET);
 
 $stmt = $conn->stmt_init();
 
-$query = "SELECT posts.*, users.firstname, users.lastname, categories.cat_name FROM posts
+$query = "SELECT posts.*, users.firstname, users.lastname, categories.cat_name 
+			FROM posts WHERE post_id='" . mysql_real_escape_string($_GET['id']) . "'
             LEFT JOIN users ON posts.user_id = users.user_id
             LEFT JOIN categories ON posts.cat_id = categories.cat_id
-            ORDER BY create_time ";
+            ";
 
 if ( mysqli_query($conn, $query) ) {
 }
@@ -21,10 +21,9 @@ if($stmt->prepare($query)) {
 	$stmt->bind_result($postId, $createTime, $editTime, $title, $text, $isPublished, $userId, $catId, $firstName, $lastName, $catName);
 
 	while(mysqli_stmt_fetch($stmt)) {
-
-        // Only displaying published posts
-        if(isset($isPublished) && $isPublished == TRUE) {
-        ?>
+		
+	
+	?>
         <div class="blogpost">
             <h1><?php echo $title; ?></h1>
             <div class="date"><p><?php echo $createTime; ?></p></div>
@@ -35,21 +34,28 @@ if($stmt->prepare($query)) {
                 echo "<p>Kategori: $catName</p>";
                 ?>
             </div>
-            <div class="comments">
+            <div class="comments"> 
             <?php 
-                echo "<a href='post.php?$postId' name='btn' >";
+                echo "<a href='post.php?id=$postId'>";
                 echo "(X) Kommentarer </a>"; 
             ?>
             </div>
         </div>
-        <?php
-        }
-	}		
-}	
+  
+		<form method="POST" action="post.php">
+			<h2>Kommentera</h2>
+			<input type="name" name="comment_name"><br>
+			<textarea rows="5" cols="30" name="blogpost_text"></textarea><br>
+			<input type="text" name="comment_text"><br>
+			<textarea rows="15" cols="80" name="blogpost_text"></textarea><br>
+			<input name="publish" class="btn btn-lg btn-primary btn-block" type="submit" value="Publicera kommentar">
+		</form>
 
+      <?php   
+	}
+}
+//}
 
-require "footer.php";
-?>		
+include "footer.php";
 
-</div>
-<div class="col-md-2"></div>
+?>
