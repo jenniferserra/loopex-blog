46d651f514f6c8fdf8f2ff4232b31dcbc8dc12db
fridalovborg <frida.lovborg@medieinstitutet.se>
2016-11-23 12:14:41 +0100

    Kommentar kopplat till databasen, kommentarer kan nu sparas. Jobbar på att skriva/publicera ut inlägg

--- post.php
+++ post.php
@@ -33,18 +33,50 @@ if($stmt->prepare($query)) {
         </div>
         <div class="comments_to_post">
             <h2>Kommentarer</h2>
-    		<form method="POST" action="post.php">
+    		<form method="POST" action="post.php?id=140">
     			<p>Namn</p>
-                <input type="text" name="blogpost_title"><br>
+                <input type="text" name="comment_name"><br>
                 <p>E-post</p>
-                <input type="text" name="blogpost_title"><br>
+                <input type="text" name="comment_epost"><br>
                 <p>Kommentar</p>
-    			<textarea rows="5" cols="30" name="blogpost_text"></textarea><br>
+    			<textarea rows="5" cols="30" name="comment_text"></textarea><br>
     			<input name="publish" class="btn btn-lg btn-primary btn-block" type="submit" value="Publicera kommentar">
     		</form>
         </div>
-      <?php   
-	}
+    <?php
+    }
+    if(isset($_POST["publish"])) {
+        if( !empty($_POST["comment_name"]) &&
+            !empty($_POST["comment_epost"]) &&
+            !empty($_POST["comment_text"])) {
+
+            // Preparing the statement
+            $stmt = $conn->stmt_init();
+            
+            // Stripping off harmful characters
+            $c_name = mysqli_real_escape_string($conn, $_POST["comment_name"]);
+            $c_epost = mysqli_real_escape_string($conn, $_POST["comment_epost"]);
+            $c_text = mysqli_real_escape_string($conn, $_POST["comment_text"]);
+
+            $timeStamp = date("Y-m-d H:i:s");
+
+            // Upload post into database. Published = TRUE
+            $query = "INSERT INTO comments VALUES ('', '{$timeStamp}', '', '{$c_text}', '{$c_epost}', '')";
+            // header("Refresh:0");
+            if ( mysqli_query($conn, $query)) {
+                    echo "Ditt inlägg är sparat i databasen";
+            } else {echo "Inlägget är inte sparat i databasen";}
+        } else { echo "Du har inte fyllt i alla fält eller valt kategori"; } 
+    }
+    ?>
+     <div class="blogpost">
+        <h1><?php echo $c_name; ?></h1>
+        <div class="date"><p><?php echo $c_epost; ?></p></div>
+        <div class="text"><p><?php echo $c_text; ?></p></div>
+        <div class="author"><p>Written by:
+        </div>
+    </div>
+<?php
 }
 include "footer.php";
 ?>
