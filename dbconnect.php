7ed2b5a33af527971006ee986e9dfc579aa42b86
jessica <jessica@none.se>
2016-11-24 14:15:35 +0100

    Removed dbconnect from git repo

--- dbconnect.php
+++
@@ -1,9 +0,0 @@
-<?php
-// ADJUST DATABASE CONFIGURATION HERE
-define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "");
-define("DB_NAME", "db_blogg");
-
-$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-?>
