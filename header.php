5b1c898a88cd2122daae8ea9397eb7dbe21e332e
fridalovborg <frida.lovborg@medieinstitutet.se>
2016-11-29 12:00:42 +0100

    no message

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <title>Login/Registrera</title>
-    <link rel="stylesheet" href="normalize.css" />
+    <link rel="stylesheet" href="normalize.css"/>
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
