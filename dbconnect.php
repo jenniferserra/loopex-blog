93810fb7d145396f8bc4c263826fd5be51a29353
fridalovborg <frida.lovborg@medieinstitutet.se>
2016-11-29 14:17:11 +0100

    Add files via upload

---
+++ dbconnect.php
@@ -0,0 +1,10 @@
+<?php
+// ADJUST DATABASE CONFIGURATION HERE. 
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "db_blogg");
+
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+// This is a comment
+?>
