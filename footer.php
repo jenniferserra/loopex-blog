0f6ae509e15a55b28c9f89d97283aaeb5f250d5e
jenniferserra <jennifer.serra@medieinstitutet.se>
2016-12-07 14:18:21 +0100

    uppdaterade footer med copyright och sociala medier

--- footer.php
+++ footer.php
@@ -1,2 +1,36 @@
 </body>
+
+
+	<footer>
+	
+		<div class="footer">
+			<div class="row">
+				<div class="col-xs-12 col-sm-6">
+					<p>Copyright Loopex 2016</p>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-xs-12 col-sm-6">
+					<p>Dela på sociala medier</p>
+
+					<!-- BILDERNA NEDAN FUNKAR INTE??? -->
+					<a href="www.facebook.com">
+					<img title="Facebook" alt="Facebook" src="/img/links/facebook.png" width="16px" height="16px" />
+					</a>
+
+					<a href="www.instagram.com">
+					<img title="Instagram" alt="Instagram" src="/img/links/instagram.png" width="16px" height="16px" />
+					</a>
+
+					<a href="www.twitter.com">
+					<img title="Twitter" alt="Twitter" src="/img/links/twitter.png" width="16px" height="16px" />
+					</a>
+				</div>
+			</div>		
+		</div>
+
+	</footer>
+
+
 </html>
