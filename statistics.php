fb2ff2d2f202009d188781ba45cead565972db63
jenniferserra <jennifer.serra@medieinstitutet.se>
2016-12-07 11:17:20 +0100

    statistics.php där man ser hur många inlägg man skrivit och kommentarer man fått. saknar bara kommentarer/inlägg

--- statistics.php
+++ statistics.php
@@ -23,6 +23,8 @@ echo "Du har " . $resArray['comment_count'] . " kommentarer.";
 require "footer.php";
 
 
+
+
 //detta kan man använda om man har en superuser som vill se vad alla användarna har för post count
 
 //SELECT user_id, count(*) as postcount_per_user FROM posts WHERE user_id in (SELECT user_id FROM users) GROUP BY user_id
