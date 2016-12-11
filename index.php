556398b89de4705cd081966a7ad61d695d472ea2
ltjn <jonathan.lampa1@gmail.com>
2016-12-11 01:38:34 +0100

    Pagination implemented and listing only five blog posts per page

--- index.php
+++ index.php
@@ -1,61 +1,179 @@
 <?php
 require "header.php";
 ?>
-<div class="banner"> </div>
-<div class="col-md-2"></div> <div class="col-md-8">
+    <div class="banner"> </div>
+    <div class="col-md-2"></div> <div class="col-md-8">
 <?php
-$stmt = $conn->stmt_init();
 
-$query = "SELECT posts.*, users.firstname, users.lastname, users.email, categories.cat_name FROM posts
+
+$sql = "SELECT posts.*, users.firstname, users.lastname, users.email, categories.cat_name FROM posts
             LEFT JOIN users ON posts.user_id = users.user_id
             LEFT JOIN categories ON posts.cat_id = categories.cat_id
+            WHERE is_published = TRUE
             ORDER BY create_time DESC";
 
-if ( mysqli_query($conn, $query) ) {
+$query = mysqli_query($conn, $sql);
+
+$post = mysqli_fetch_row($query);
+$posts = $post[0];
+$postsPerPage = 5;
+
+// Tells the page nr of the very last page ("ceil" rounds numbers up)
+$last = ceil($posts/$postsPerPage);
+$first = 1;
+
+// Makes sure that last page cannot be less than 1
+if ($last < 1) {
+    $last = 1;
 }
-if($stmt->prepare($query)) {
-    $stmt->execute();
-    $stmt->bind_result($postId, $createTime, $editTime, $title, $text, $isPublished, $userId, $catId, $firstName, $lastName, $user_email, $catName);
 
-    while(mysqli_stmt_fetch($stmt)) {
+// If no page-number URL-variables are available
+$pageNumber = 1;
 
-        // Only displaying published posts
-        if(isset($isPublished) && $isPublished == TRUE) {
+// Replacing pagenumber in url
+if(isset($_GET['pn'])) {
+    $pageNumber = preg_replace('#[^0-9]#', '', $_GET['pn']);
+}
 
-        ?>
-        <div class="blogpost_center">
-            <div class="blogpost">
-                <h1><?php echo $title; ?></h1>
-                <div class="date"><p><?php echo $createTime; ?></p></div>
-                <div class="text"><p><?php echo $text; ?></p></div>
-                <div class="text"><p><?php echo "<p>Kategori: $catName</p>"; ?></p></div>
-                <div class="author"><p>Skriven av:
-                    <?php
-                    echo "<a href='author.php?id=$userId'>$firstName $lastName</p></a>";
-                    echo "<p><a href='mailto:$user_email'>$user_email</a></p>";
-                    ?>
-                </div>
-                <div class="comments">
-                <?php 
-                    echo "<a href='post.php?id=$postId' name='btn'>";
-                    echo "(X) Kommentarer </a>"; 
+
+if($pageNumber < 1) {
+    $pageNumber = 1;
+} elseif ($pageNumber > $last) {
+    $pageNumber = $last;
+}
+
+// Query for a limited amount of posts
+$limit = 'LIMIT ' .($pageNumber - 1) * $postsPerPage .',' . $postsPerPage;
+
+$sql = "SELECT posts.*, users.firstname, users.lastname, users.email, categories.cat_name FROM posts
+        LEFT JOIN users ON posts.user_id = users.user_id
+        LEFT JOIN categories ON posts.cat_id = categories.cat_id
+        WHERE is_published = TRUE
+        ORDER BY create_time DESC $limit";
+
+$query = mysqli_query($conn, $sql);
+
+
+// Establishing the $paginationCtrls variable
+$paginationCtrls = '';
+
+// If there is more than one page worth of results
+if ($last !=1) {
+    if ($pageNumber > 1) {
+        $previous = $pageNumber - 1;
+        $fillNumbersBehind = -4;
+        $jumpBackward = $pageNumber - 4;
+
+        if ($pageNumber >= $last - 4){
+            $fillNumbersBehind = $pageNumber - $last;
+            $jumpBackward = $pageNumber - 8 + $fillNumbersBehind;
+        }
+
+        // Previous-button and long-backward-jump
+        $paginationCtrls .= '<a href="' .$_SERVER['PHP_SELF'] . '?pn=' . $jumpBackward . '"> << </a> &nbsp
+        <a href="' . $_SERVER['PHP_SELF'] . '?pn=' . $previous . '">Previous</a> &nbsp;';
+        
+
+
+        // LEFT - Render clickable number links to the left
+        for($i = $pageNumber-8-$fillNumbersBehind; $i < $pageNumber; $i++) {
+            if ($i > 0) {
+                $paginationCtrls .= '<a href="' .$_SERVER['PHP_SELF'] . '?pn=' . $i . '">' . $i . '</a> &nbsp; ';
+
+            }
+        }
+    }
+
+    // CURRENT PAGE - Render the target page number (not being a link)
+    $paginationCtrls .= ''.$pageNumber.' &nbsp; ';
+
+    // RIGHT - Render clickable number links that should appear on the right
+    for ($i = $pageNumber+1; $i <= $last; $i++) {
+        $paginationCtrls .= '<a href="' . $_SERVER['PHP_SELF'] . '?pn=' . $i . '">' . $i . '</a> &nbsp; ';
+        
+        // Making the index always show the same amount of page links
+        if ($pageNumber <= 4){
+            $fillNumbersInfront= 5 - $pageNumber;
+        } else {
+            $fillNumbersInfront= 0;
+        }
+        // End of pagination links
+        if ($i >= $pageNumber + 4 + $fillNumbersInfront) {
+            break;
+        }
+    }
+
+    // Next-button and long-forward-jump button
+    if ($pageNumber != $last) {
+        $next = $pageNumber + 1;
+        $jumpForward = $pageNumber + 4 + $fillNumbersInfront;
+        $paginationCtrls .= '&nbsp; <a href="' .$_SERVER['PHP_SELF'] . '?pn=' . $next . '">Next</a> &nbsp
+        <a href="' .$_SERVER['PHP_SELF'] . '?pn=' . $jumpForward . '"> >> </a> ';
+    }
+}
+
+// Looping out blog posts a few at a time
+while ($post = mysqli_fetch_array($query, MYSQLI_ASSOC)) {
+    $postId = $post["post_id"];
+    $createTime = $post["create_time"];
+    $editTime = $post["edit_time"];
+    $title = $post["title"];
+    $text = $post["text"];
+    $isPublished = $post["is_published"];
+    $userId = $post["user_id"];
+    $catId = $post["cat_id"];
+    $firstName = $post["firstname"];
+    $lastName = $post["lastname"];
+    $user_email = $post["email"];
+    $catName = $post["cat_name"];
+
+
+    // Only displaying published posts
+    if(isset($isPublished) && $isPublished == TRUE) {
+
+    ?>
+
+    <div class="blogpost_center">
+        <div class="blogpost">
+            <h1><?php echo $title; ?></h1>
+            <div class="date"><p><?php echo $createTime; ?></p></div>
+            <div class="text"><p><?php echo $text; ?></p></div>
+            <div class="text"><p><?php echo "<p>Kategori: $catName</p>"; ?></p></div>
+            <div class="author"><p>Skriven av:
+                <?php
+                echo "<a href='author.php?id=$userId'>$firstName $lastName</p></a>";
+                echo "<p><a href='mailto:$user_email'>$user_email</a></p>";
                 ?>
-                </div>
-                <div class="edit">
-                <?php 
-                if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] == TRUE && $_SESSION["user_id"] == $userId) {
-                    echo "<a href='editpost.php?editid=$postId' name='btn'>";
-                    
-                    echo "Redigera </a>";
-                }
-                ?>
-                </div>
+            </div>
+            <div class="comments">
+            <?php 
+                echo "<a href='post.php?id=$postId' name='btn'>";
+                echo "(X) Kommentarer </a>"; 
+            ?>
+            </div>
+            <div class="edit">
+            <?php 
+            if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] == TRUE && $_SESSION["user_id"] == $userId) {
+                echo "<a href='editpost.php?editid=$postId' name='btn'>";
+                
+                echo "Redigera </a>";
+            }
+            ?>
             </div>
         </div>
-        <?php
-        }
-    }       
-}   
+    </div>
+    <?php
+   }
+}
+//Closing database connection
+mysqli_close($conn);
+?>
+
+<div>
+    <div id="pagination_controls"><?php echo $paginationCtrls; ?></div>
+</div>
+
+<?php 
 require "footer.php";
 ?>      
 </div>
