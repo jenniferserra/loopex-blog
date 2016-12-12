2fb927aede953d7c1410d3905320062132fd4bd3
jenniferserra <jennifer.serra@medieinstitutet.se>
2016-12-12 11:20:01 +0100

    fixat bakgrunder, nu funkar media sizes, footer bugg fixad

--- footer.php
+++ footer.php
@@ -1,33 +1,33 @@
-</body>
-	<footer>
+		</div> <!-- End of page-content class -->
+		<footer>
 
-		<div class="footer">
-			<div class="container-fluid">
-			<div class="row">
-				<div class="col-xs-12 col-sm-12">
-					<p>Copyright Loopex <?php echo date("Y") ?> </p>
+			<div class="footer">
+				<div class="container-fluid">
+					<div class="row">
+						<div class="col-xs-12 col-sm-12">
+							<p>Copyright Loopex <?php echo date("Y") ?> </p>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-xs-12 col-sm-12">
+							<p>Dela på sociala medier</p>
+
+							<a href="http://www.facebook.com">
+		           				<i class="fa fa-facebook-square fa-3x" aria-hidden="true"></i>
+							</a>
+
+							<a href="http://www.instagram.com">
+		           				<i class="fa fa-instagram fa-3x" aria-hidden="true"></i>
+							</a>
+		          			<a href="http://www.twitter.com">
+		          				<i class="fa fa-twitter-square fa-3x" aria-hidden="true"></i>
+		          			</a>
+						</div>
+					</div>
 				</div>
 			</div>
-
-			<div class="row">
-				<div class="col-xs-12 col-sm-12">
-					<p>Dela på sociala medier</p>
-
-					<a href="http://www.facebook.com">
-            <i class="fa fa-facebook-square fa-3x" aria-hidden="true"></i>
-					</a>
-
-					<a href="http://www.instagram.com">
-            <i class="fa fa-instagram fa-3x" aria-hidden="true"></i>
-					</a>
-          <a href="http://www.twitter.com"><i class="fa fa-twitter-square fa-3x" aria-hidden="true"></i></a>
-					</a>
-				</div>
-			</div>
-		</div>
-	</div>
-
-	</footer>
-
+		</footer>
+	</body>
 
 </html>
