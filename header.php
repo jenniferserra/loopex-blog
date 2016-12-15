e96cb850d239ae2e5f16a897da7007621ae4aada
fridalovborg <frida.lovborg@medieinstitutet.se>
2016-12-15 09:43:30 +0100

    fix header

--- header.php
+++ header.php
@@ -4,11 +4,11 @@
     <meta charset="utf-8">
     <title>Loopex bloggportal</title>
     <link rel="stylesheet" href="normalize.css"/>
-    <link rel="stylesheet" href="style/css/style.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="style/css/style.css">
 </head>
-<body>
+<body class="dashboard comments drafts archive statistics index">
 <?php
 require_once "dbconnect.php";
 session_start();
@@ -17,7 +17,7 @@ if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == TRUE ) {
     // IF LOGGED IN
     // ------------------------------------------------------------------------
     $userid = $_SESSION['user_id'];
-    $stmt = $conn->stmt_init();
+    // $stmt = $conn->stmt_init();
     $stmt->prepare("SELECT * FROM users WHERE user_id = '{$userid}'");
     $stmt->execute();
     $stmt->bind_result($user_id, $firstname, $lastname, $email, $encrypt_password, $profilepic, $role);
@@ -35,10 +35,14 @@ if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == TRUE ) {
                 <a class="navbar-brand" href="drafts.php">Utkast</a>
                 <a class="navbar-brand" href="statistics.php">Statistik</a>
             </div>
+            <div class="navbar-header navbar-right">
+                <a class="navbar-brand" href="logout.php">Logga ut</a>
+            </div>
         </nav>
     </header>
 
     <?php
+    $stmt->close();
        // ---------------------------------------------------------------------
         // IF NOT LOGGED IN
         // --------------------------------------------------------------------
@@ -72,7 +76,9 @@ if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == TRUE ) {
                 </div>
             </nav>
         </header>
+
+        <!-- start a wrapper -->
+        <div class="page-content">
     <?php
         }
-    $stmt->close();
 ?>
