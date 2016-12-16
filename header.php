2bd86b3e5bbb33ca5a691e0fad51c463862f2c4a
ltjn <jonathan.lampa1@gmail.com>
2016-12-16 13:23:21 +0100

    header in logged in mode adjusted

--- header.php
+++ header.php
@@ -30,14 +30,14 @@ if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == TRUE ) {
     $stmt->fetch();
 ?>
 
-                    <li class="navbar-brand menu-btn-lvl-1"><a href="index.php">Bloggen</a></li>
-                    <li class="navbar-brand menu-btn-lvl-1"><a href="dashboard.php">Profil</a></li>
-                    <li class="navbar-brand menu-btn-lvl-1"><a href="comments.php">Kommentarer</a></li>
-                    <li class="navbar-brand menu-btn-lvl-1"><a href="archive.php">Arkiv</a></li>
-                    <li class="navbar-brand menu-btn-lvl-1"><a href="drafts.php">Utkast</a></li>
-                    <li class="navbar-brand menu-btn-lvl-1"><a href="dashboard.php">Profil</a></li>
-                    <li class="navbar-brand menu-btn-lvl-1"><a href="statistics.php">Statistik</a></li>
-                    <li class="navbar-brand menu-btn-lvl-1"><a href="logout.php">Logga ut</a></li>
+                    <li class="menu-btn-lvl-1"><a href="index.php">Bloggen</a></li>
+                    <li class="menu-btn-lvl-1"><a href="dashboard.php">Profil</a></li>
+                    <li class="menu-btn-lvl-1"><a href="comments.php">Kommentarer</a></li>
+                    <li class="menu-btn-lvl-1"><a href="archive.php">Arkiv</a></li>
+                    <li class="menu-btn-lvl-1"><a href="drafts.php">Utkast</a></li>
+                    <li class="menu-btn-lvl-1"><a href="dashboard.php">Profil</a></li>
+                    <li class="menu-btn-lvl-1"><a href="statistics.php">Statistik</a></li>
+                    <li class="menu-btn-lvl-1"><a href="logout.php">Logga ut</a></li>
 
     <?php
     $stmt->close();
