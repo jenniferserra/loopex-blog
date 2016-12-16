e397315bf80e28553f7188c5d931beb58eee1f06
ltjn <jonathan.lampa1@gmail.com>
2016-12-16 23:15:13 +0100

    changes in header.php. btn-files removed

--- header_btns/default_outlog_btns.php
+++
@@ -1,6 +0,0 @@
-                    <li class="menu-btn-lvl-1"><a href="index.php">Hem</a></li>
-                    <div class="navbar-header navbar-right">
-                        <li class="menu-btn-lvl-1">
-                            <a class="navbar-brand" href="login.php">Logga in</a>
-                        </li>
-                    </div>
