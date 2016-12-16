19c6a50debabc99d68d3bd0cc61b123da902b094
ltjn <jonathan.lampa1@gmail.com>
2016-12-16 13:29:16 +0100

    link to index.php when cklickin on  hem-btn

--- header.php
+++ header.php
@@ -47,7 +47,7 @@ if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == TRUE ) {
 
     } else {
         ?>
-                    <li class="menu-btn-lvl-1"><a href="#">Hem</a></li>
+                    <li class="menu-btn-lvl-1"><a href="index.php">Hem</a></li>
                     <li class="menu-btn-lvl-1"><a href="?category=0">Kategori</a>
                         <ul>
                             <li class="menu-btn-lvl-2"><a class="nav-link" href="?category=1">Sport</a></li>
