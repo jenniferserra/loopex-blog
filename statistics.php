5a61c27f37382e00bdf914802e7ae867801577c4
jessica <jessica@none.se>
2016-12-23 10:11:48 +0100

    Bakgrundsbild på statistik sidan

--- statistics.php
+++ statistics.php
@@ -6,6 +6,7 @@
 require "header.php";
 ?>
 	<div class="statistics-box col-xs-12">
+		<body class="imgStatistics">
 		<h1 class="statistics">Statistik</h1>
 		<?php
 		/* ----------------------------------------------------------------------------
@@ -44,13 +45,13 @@ require "header.php";
 		echo '<div class="statistics-count">Du har '. $nbr . ' kommentarer per inlägg.</div>';
 		?>
 	</div> <!-- .statistics-box.col-xs-12 -->
+</div> <!-- END OF IMG STATISTIC -->
 
 <?php
 	require "footer.php";
 
-	// TO DO!! ? 
+	// TO DO!! ?
 	//
 	//detta kan man använda om man har en superuser som vill se vad alla användarna har för post count
 	//SELECT user_id, count(*) as postcount_per_user FROM posts WHERE user_id in (SELECT user_id FROM users) GROUP BY user_id
 ?>
-	
