47df2d5d664c45365966c19e20334f11067e8940
fridalovborg <frida.lovborg@medieinstitutet.se>
2016-12-26 14:52:56 +0100

    bakgrundbild + filändringsfix!!

--- superuser.php
+++ superuser.php
@@ -1,4 +1,12 @@
-<?php include "header.php";
+<?php
+require_once "code_open.php";
+?>
+<body class="statistics">
+  <!-- start a wrapper -->
+  <div class="page-content">
+  <?php
+  require_once "header.php";
+  ?>
 $stmt = $conn->stmt_init();
       include "functions/functions.php";
 ?>
