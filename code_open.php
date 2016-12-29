3eac4834bb6d38728be189aa14bdbbe567eeba0c
ltjn <jonathan.lampa1@gmail.com>
2016-12-29 14:59:58 +0100

    requiring functions on all pages

--- code_open.php
+++ code_open.php
@@ -7,3 +7,11 @@
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="style/css/style.css">
 </head>
+
+
+<?php
+//-----------------------------------------------------------------------------
+// Requiring functions for all pages
+//-----------------------------------------------------------------------------
+require "functions/functions.php";
+?>
